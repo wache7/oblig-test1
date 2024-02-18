ad49fe2a145f37ba6474ad7a8c07ba57222f8a3d
wache7 <wahway777@gmail.com>
2024-02-18 17:50:00 +0100

    jj

--- oblig1.js
+++ oblig1.js
@@ -1,22 +1,21 @@
 let billettRegister = []; // lager en array som skal lagre opplysninger til en person som skal kjøpe en billett.
+// henter verdiene fra input ved å peke på id til dem.
+const film = document.getElementById("film").value; // verdien av nedtrykkslisten.
+const antall = document.getElementById("antall").value; // feilmelding?
+const fornavn = document.getElementById("fornavn").value;
+const etternavn = document.getElementById("etternavn").value;
+const telefonnr = document.getElementById("telefonnr").value;
+const epost = document.getElementById("epost").value;
+
+let meldingFilmEL = document.getElementById("film-feilmelding");
+let meldingAntallEl = document.getElementById("antall-feilmelding");
+let meldingFornavnEl = document.getElementById("fornavn-feilmelding"); // bruke innerhtml senere etter å ha opprettet strukturen til tabellen.
+let meldingEtternavnEl = document.getElementById("etternavn-feilmelding");
+let meldingTelefonnrEl = document.getElementById("telefonnr-feilmelding"); // if(isNaN) --> alert. else.
+let meldingEpostEl = document.getElementById("e-post-feilmelding"); // if
 
 let outputEl = document.getElementById("output");
 function registrerBillett() {
-  // henter verdiene fra input ved å peke på id til dem.
-  const film = document.getElementById("film").value; // verdien av nedtrykkslisten.
-  const antall = document.getElementById("antall").value; // feilmelding?
-  const fornavn = document.getElementById("fornavn").value;
-  const etternavn = document.getElementById("etternavn").value;
-  const telefonnr = document.getElementById("telefonnr").value;
-  const epost = document.getElementById("epost").value;
-
-  let meldingFilmEL = document.getElementById("film-feilmelding");
-  let meldingAntallEl = document.getElementById("antall-feilmelding");
-  let meldingFornavnEl = document.getElementById("fornavn-feilmelding"); // bruke innerhtml senere etter å ha opprettet strukturen til tabellen.
-  let meldingEtternavnEl = document.getElementById("etternavn-feilmelding");
-  let meldingTelefonnrEl = document.getElementById("telefonnr-feilmelding"); // if(isNaN) --> alert. else.
-  let meldingEpostEl = document.getElementById("e-post-feilmelding"); // if
-
   let booleanValue = true;
   // skal lage et objekt som skal settes inn i bilettRegister-arrayet ved bruk av push omsider.
 
