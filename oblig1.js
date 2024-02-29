42e5127a8a702e8e4e62804e39885b00bc80174f
wache7 <wahway777@gmail.com>
2024-02-29 14:24:20 +0100

    29

--- oblig1.js
+++ oblig1.js
@@ -1,22 +1,66 @@
 let billettRegister = []; // lager en array som skal lagre opplysninger til en person som skal kjøpe en billett.
-// henter verdiene fra input ved å peke på id til dem.
-const film = document.getElementById("film").value; // verdien av nedtrykkslisten.
-const antall = document.getElementById("antall").value; // feilmelding?
-const fornavn = document.getElementById("fornavn").value;
-const etternavn = document.getElementById("etternavn").value;
-const telefonnr = document.getElementById("telefonnr").value;
-const epost = document.getElementById("epost").value;
+// henter verdiene fra input ved å peke på id-en til dem.
 
-let meldingFilmEL = document.getElementById("film-feilmelding");
-let meldingAntallEl = document.getElementById("antall-feilmelding");
-let meldingFornavnEl = document.getElementById("fornavn-feilmelding"); // bruke innerhtml senere etter å ha opprettet strukturen til tabellen.
-let meldingEtternavnEl = document.getElementById("etternavn-feilmelding");
-let meldingTelefonnrEl = document.getElementById("telefonnr-feilmelding"); // if(isNaN) --> alert. else.
-let meldingEpostEl = document.getElementById("e-post-feilmelding"); // if
-
-let outputEl = document.getElementById("output");
 function registrerBillett() {
-  let booleanValue = true;
+  const film = document.getElementById("film").value; // henter verdien av nedtrykkslisten.
+  const antall = document.getElementById("antall").value; //
+  const fornavn = document.getElementById("fornavn").value;
+  const etternavn = document.getElementById("etternavn").value;
+  const telefonnr = document.getElementById("telefonnr").value;
+  const epost = document.getElementById("epost").value;
+
+  let booleanValue = true; // lager en type boolean som skal være essensielt for å fange mulige.
+
+  // henter div elementene ved bruk av document.getElementbyId((navnet til diverse divs))
+  let meldingFilmEL = document.getElementById("film-feilmelding");
+  let meldingAntallEl = document.getElementById("antall-feilmelding");
+  let meldingFornavnEl = document.getElementById("fornavn-feilmelding"); // bruke innerhtml senere etter å ha opprettet strukturen til tabellen.
+  let meldingEtternavnEl = document.getElementById("etternavn-feilmelding");
+  let meldingTelefonnrEl = document.getElementById("telefonnr-feilmelding"); // if(isNaN) --> alert. else.
+  let meldingEpostEl = document.getElementById("e-post-feilmelding"); // if
+
+  if (isNaN(antall) || antall === "") {
+    console.log("Vi er i input validering for antall");
+    meldingAntallEl.innerHTML = "Du må skrive et heltall";
+    booleanValue = false;
+  } else {
+    meldingAntallEl.innerHTML = "";
+  }
+
+  if (fornavn === "" || !isNaN(fornavn)) {
+    meldingFornavnEl.innerHTML = "Du må skrive navnet ditt";
+    booleanValue = false;
+  } else {
+    meldingFornavnEl.innerHTML = "";
+  }
+
+  if (etternavn === "" || !isNaN(etternavn)) {
+    meldingEtternavnEl.innerHTML = "Du må skrive etternavnet ditt";
+    booleanValue = false;
+  } else {
+    meldingEtternavnEl.innerHTML = "";
+  }
+
+  if (isNaN(telefonnr) || telefonnr === "") {
+    meldingTelefonnrEl.innerHTML = "Du må skrive telefonnummeret ditt";
+    booleanValue = false;
+  } else {
+    meldingTelefonnrEl.innerHTML = "";
+  }
+
+  if (epost === "") {
+    meldingEpostEl.innerHTML = "Du må skrive eposten din";
+    booleanValue = false;
+  } else {
+    meldingEpostEl.innerHTML = "";
+  }
+
+  //let outputEl = document.getElementById("output"); // skal skrive ut tabellen.
+
+  console.log("vi er i registrer bilett");
+
+  console.log(fornavn);
+
   // skal lage et objekt som skal settes inn i bilettRegister-arrayet ved bruk av push omsider.
 
   const person = {
@@ -27,44 +71,19 @@ function registrerBillett() {
     telefonnr: telefonnr,
     epost: epost,
   };
-
-  if (isNaN(antall) || antall.value === "") {
-    meldingAntallEl.innerHTML = "Du må skrive et heltall";
-    booleanValue = false;
-  }
-
-  if (fornavn.value === "") {
-    meldingFornavnEl.innerHTML = "Du må skrive navnet ditt";
-    booleanValue = false;
-  }
-
-  if (etternavn.value === "") {
-    meldingEtternavnEl.innerHTML = "Du må skrive etternavnet ditt";
-    booleanValue = false;
-  }
-
-  if (isNaN(telefonnr) || telefonnr.value === "") {
-    meldingTelefonnrEl.innerHTML = "Du må skrive telefonnummeret ditt";
-    booleanValue = false;
-  }
-
-  if (epost.value === "") {
-    meldingEpostEl.innerHTML = "Du må skrive eposten din";
-    booleanValue = false;
-  }
-
-  if (booleanValue === true) {
+  if (booleanValue) {
     billettRegister.push(person);
     lagTabell();
   }
 }
 
 function lagTabell() {
+  // oppretter strukturen til en tabell.
   let ut = "<table><tr>";
   ut +=
     "<th> Film </th><th> Antall</th><th> Fornavn </th><th> Etternavn </th> <th> Telefonnr</th><th>E-post</th>";
   ut += "</tr>";
-
+  // p går gjennom billettregister-arrayet som inneholder objekter.
   for (let p of billettRegister) {
     ut += "<tr>";
     ut +=
@@ -83,10 +102,12 @@ function lagTabell() {
       "</td>";
   }
 
-  outputEl.innerHTML = ut + "hei";
+  ut += "</table>";
+  document.getElementById("output").innerHTML = ut;
 }
 
 function slettBillett() {
+  // henter verdien til input, nedtrykksliste og fjerner verdien ved å sette dem slik = ""; ingenting i value.
   document.getElementById("film").value = "";
   document.getElementById("antall").value = "";
   document.getElementById("fornavn").value = "";
@@ -94,9 +115,9 @@ function slettBillett() {
   document.getElementById("telefonnr").value = "";
   document.getElementById("epost").value = "";
 
-  billettRegister = [];
+  billettRegister = []; // nullstiller arrayet som inneholder objekter som fremvises i tabellen.
 
-  outputEl.innerHTML = "";
+  document.getElementById("output").innerHTML = "";
 }
 
 // slett knappen.
